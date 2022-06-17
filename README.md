aa6589013088e3322d48b3666ad7e491f2751406
botprzemek <92102878+botprzemek@users.noreply.github.com>
2022-06-17 09:45:15 +0200

    LaunchPad Fix and README update

--- README.md
+++ README.md
@@ -1,2 +1,45 @@
 # bpLobby
-botprzemek's Lobby plugin for Spigot
+
+## _botprzemek's Lobby plugin for Spigot 1.18+_<br><br>
+
+## Reading from config and %player% placeholder :heavy*check_mark: \*\*\_3/3*\*\*
+
+1. Config.yml :heavy_check_mark:
+2. %player% into .getPlayer(); :heavy_check_mark:
+3. Reloading on alias :heavy_check_mark:<br>
+
+## HEX colors and Gradients with IridiumColorAPI and HEX to RGB :heavy*check_mark: \*\*\_2/2*\*\*
+
+1. IridiumColorAPI.process :heavy_check_mark:
+2. HEX to RGB for fireworks and armor :heavy_check_mark:
+
+## On Join and Quit :grey*exclamation: \*\*\_3/4*\*\*
+
+1. Message :heavy_check_mark:
+2. Title :heavy_check_mark:
+3. Fireworks :heavy_check_mark:
+4. Particles :x:<br>
+
+## Launch pad :grey*exclamation: \*\*\_2/3*\*\*
+
+1. Message :heavy_check_mark:
+2. Fixed double trigger :heavy_check_mark:
+3. Particles :x:<br>
+
+## Fly command :heavy*check_mark: \*\*\_3/3*\*\*
+
+1. Message :heavy_check_mark:
+2. Set speed :heavy_check_mark:
+3. Player ON/OFF :heavy_check_mark:<br>
+
+## Gamemode command :grey*exclamation: \*\*\_0/0*\*\*
+
+1. Message :x:
+1. Alias :x:
+1. GUI :x:<br>
+
+## Menu :grey*exclamation: \*\*\_0/0*\*\*
+
+1. Command :x:
+2. Open on item :x:
+3. GUI :x:<br>
--- src/main/java/pl/botprzemek/handlers/LaunchPad.java
+++ src/main/java/pl/botprzemek/handlers/LaunchPad.java
@@ -4,6 +4,7 @@ import com.iridium.iridiumcolorapi.IridiumColorAPI;
 import org.bukkit.Bukkit;
 import org.bukkit.Location;
 import org.bukkit.Material;
+import org.bukkit.Sound;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
@@ -20,11 +21,13 @@ public class LaunchPad implements Listener {
 
     float launchPadPower = bpLobby.plugin.getConfig().getInt("launch-pad.power");
     int launchPadMax = bpLobby.plugin.getConfig().getInt("launch-pad.max");
+    int wait = 0;
     Boolean launchPadEnable = bpLobby.plugin.getConfig().getBoolean("launch-pad.enable");
     Material launchPadActivator = Material.valueOf(Objects.requireNonNull(bpLobby.plugin.getConfig().getString("launch-pad.activator")).toUpperCase().replace(" ", "_"));
     Material launchPadMaterial = Material.valueOf(Objects.requireNonNull(bpLobby.plugin.getConfig().getString("launch-pad.material")).toUpperCase().replace(" ", "_"));
     String prefix = bpLobby.plugin.getConfig().getString("prefix");
     String launchPadMessage = bpLobby.plugin.getConfig().getString("messages.launch-pad.activate");
+    String sound = Objects.requireNonNull(bpLobby.plugin.getConfig().getString("launch-pad.sound")).toUpperCase().replace(' ', '_');
 
     @EventHandler
     public void onWalking(PlayerMoveEvent event){
@@ -35,9 +38,15 @@ public class LaunchPad implements Listener {
             Player player = event.getPlayer();
             Location location = player.getLocation();
             location.setY(location.getBlockY() - 1);
+
             if(player.getLocation().getBlock().getType().equals(launchPadActivator) && location.getBlock().getType().equals(launchPadMaterial)){
-                player.sendMessage(IridiumColorAPI.process(prefix + launchPadMessage));
-                player.setVelocity(location.getDirection().multiply(launchPadPower).setY(launchPadMax));
+                if(wait == 0) {
+                    player.setVelocity(location.getDirection().multiply(launchPadPower).setY(launchPadMax));
+                    player.sendMessage(IridiumColorAPI.process(prefix + launchPadMessage));
+                    player.playSound(player.getLocation(), Sound.valueOf(sound), 1.0f, 1.0f);
+                    ++wait;
+                }
+                Bukkit.getScheduler().runTaskLater(bpLobby.plugin, () -> { wait = 0; }, 10L);
             }
         }
     }
