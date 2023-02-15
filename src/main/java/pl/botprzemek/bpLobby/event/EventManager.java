9681a7970a694837d093ebd03ce0037f2024f26f
botprzemek <botprzemekk@gmail.com>
2023-02-15 21:50:05 +0100

    LiteCommands

--- src/main/java/pl/botprzemek/bpLobby/event/EventManager.java
+++ src/main/java/pl/botprzemek/bpLobby/event/EventManager.java
@@ -4,22 +4,13 @@ import pl.botprzemek.bpLobby.BpLobby;
 import pl.botprzemek.bpLobby.lobby.LobbyManager;
 
 public class EventManager {
-
     public EventManager(LobbyManager lobbyManager) {
-
         BpLobby instance = lobbyManager.getInstance();
-
         instance.getServer().getPluginManager().registerEvents(new JoinQuitEvent(lobbyManager), instance);
-
         instance.getServer().getPluginManager().registerEvents(new ChatEvent(lobbyManager), instance);
-
         instance.getServer().getPluginManager().registerEvents(new InventoryEvent(lobbyManager), instance);
-
         instance.getServer().getPluginManager().registerEvents(new SpawnProtectionEvent(lobbyManager), instance);
-
-        if (lobbyManager.getConfigManager().getLobbyConfig().getLobbyLimit() != 0) instance.getServer().getPluginManager().registerEvents(new PlayerFallingEvent(lobbyManager), instance);
-
+        instance.getServer().getPluginManager().registerEvents(new PlayerFallingEvent(lobbyManager), instance);
         instance.getServer().getPluginManager().registerEvents(new KickWithReasonEvent(lobbyManager), instance);
-
     }
 }
